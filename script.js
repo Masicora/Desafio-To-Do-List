5971388eba10d67bf3c558cf492981272fdeb61d
Jonathan Bidwell <jbidwellb@gmail.com>
2022-07-24 17:13:17 -0400

    avances

--- script.js
+++ script.js
@@ -3,27 +3,33 @@ let tareasRealizadas = document.querySelector("#tareasRealizadas");
 let botonAgregar = document.querySelector("#botonAgregar");
 let listadoTareas = document.querySelector("#listadoTareas");
 let tareas = [{ id: 1658531872574, nombre: "leer" }, { id: 1658531872575, nombre: "escribir" }, { id: 1658531872576, nombre: "hacer ejercicio" }];
+let conteoRealizadas = 0;
 refrescar();
 
 function borrar(id) {
     const index = tareas.findIndex((ele) => ele.id == id);
     tareas.splice(index, 1);
-    /* Actualizamos la información en el HTML */
     refrescar();
 }
 //debo revisar esto para luego
-function checked() {
-    let check = document.querySelector("#check");
-    if (check.checked) {
-        this.style.color = "red";
-    } else {
-        this.style.color = "black";
+function check(x) {
+    if (x.checked == true) {
+        conteoRealizadas++;
+        x.parentNode.style.backgroundColor = "#82E0AA";
+
     }
+    else {
+        conteoRealizadas--;
+        x.parentNode.style.backgroundColor = "";
+
+    }
+    tareasRealizadas.innerHTML = `Realizadas: ${conteoRealizadas}`;
 }
+
+
 
 botonAgregar.addEventListener("click", function () {
     let inputTarea = document.querySelector("#inputTarea");
-
     let nuevaTarea = {
         id: Date.now(),
         nombre: inputTarea.value
@@ -35,10 +41,12 @@ botonAgregar.addEventListener("click", function () {
 function refrescar() {
     let html = "<tr><th>Id</th><th>Nombre</th><th>Realizada</th><th>Borrar</th></tr>";
     for (tarea of tareas) {
-        html += `<tr><td>${tarea.id}</td><td>${tarea.nombre}</td><td><input id="check" type="checkbox" onclick="checked()"></input></td><td><button onclick="borrar(${tarea.id})">X</button></td></tr>`;
+        html += `<tr><td>${tarea.id}</td><td>${tarea.nombre}</td><td><input type="checkbox" onchange="check(this)"></input></td><td><button onclick="borrar(${tarea.id})">X</button></td></tr>`;
     }
     listadoTareas.innerHTML = html;
     inputTarea.value = "";
+    tareasTotal.innerHTML = `Total: ${tareas.length}`;
+    conteoRealizadas = 0;
 
 
 }
